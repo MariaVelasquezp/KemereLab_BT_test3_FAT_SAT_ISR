2791432357a390f8f0c3268aa636ae5206bb0ff1
MariaVelasquezp <101753368+MariaVelasquezp@users.noreply.github.com>
2023-08-26 21:01:25 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,214 @@
+#include <project.h>
+
+typedef enum {
+    ACTIVE,
+    WAKEUP_SLEEP,
+    WAKEUP_DEEPSLEEP,
+    SLEEP,
+    DEEPSLEEP
+} ApplicationPower;
+
+ApplicationPower applicationPower;
+
+int apiResult;
+
+void AppCallBack(uint32 event, void* eventParam) 
+{ 
+    CYBLE_BLESS_CLK_CFG_PARAMS_T clockConfig; 
+     
+    switch(event) 
+    { 
+      /* Handle stack events */ 
+      case CYBLE_EVT_STACK_ON: 
+ 
+          /* C8. Get the configured clock parameters for BLE subsystem */ 
+          CyBle_GetBleClockCfgParam(&clockConfig);     
+             
+          /* C8. Set the device sleep-clock accuracy (SCA) based on the tuned ppm  
+          of the WCO */ 
+          clockConfig.bleLlSca = CYBLE_LL_SCA_000_TO_020_PPM; 
+             
+          /* C8. Set the clock parameter of BLESS with updated values */ 
+          CyBle_SetBleClockCfgParam(&clockConfig); 
+ 
+          /* Put the device into discoverable mode so that a Central device can   
+          connect to it. */ 
+          apiResult = CyBle_GappStartAdvertisement(CYBLE_ADVERTISING_FAST); 
+           
+          /* Application-specific event handling here */ 
+        
+        /*Probably we receive a RX Buff, with the transmiter data from GATT*/
+        
+        break; 
+    
+      /* Other application-specific event handling here */ 
+      case CYBLE_EVT_GAP_DEVICE_CONNECTED: 
+       // LED_Write(1);
+        break;
+    
+      case CYBLE_EVT_GAP_DEVICE_DISCONNECTED: 
+        apiResult = CyBle_GappStartAdvertisement(CYBLE_ADVERTISING_FAST); 
+        //LED_Write(0);
+        break;
+    } 
+} 
+
+inline void RunApplication() 
+{ 
+    /*********************************************************************** 
+    *  Place your application code here 
+    ************************************************************************/   
+ 
+    /* if you are done with everything and ready to go to sleep,  
+    then set it up to go to sleep. Update the code inside if() specific 
+    to your application*/   
+    if(0)  
+    { 
+        applicationPower = SLEEP; 
+    } 
+     
+    /* if you are done with everything and ready to go to deepsleep,  
+    then set it up to go to deepsleep. Update the code inside if() specific 
+    to your application*/   
+    if (1) 
+    { 
+        applicationPower = DEEPSLEEP; 
+    } 
+} 
+
+void ManageApplicationPower() 
+{ 
+    switch(applicationPower) 
+    { 
+        case ACTIVE: // don’t need to do anything 
+        break; 
+         
+        case WAKEUP_SLEEP: // do whatever wakeup needs to be done  
+         
+            applicationPower = ACTIVE; 
+        break; 
+         
+        case WAKEUP_DEEPSLEEP: // do whatever wakeup needs to be done. 
+         
+            applicationPower = ACTIVE; 
+        break; 
+ 
+        case SLEEP:  
+        /*********************************************************************** 
+         * Place code to place the application components to sleep here  
+        ************************************************************************/         
+        break; 
+         
+        case DEEPSLEEP: 
+        /*********************************************************************** 
+        *  Place code to place the application components to deepsleep here  
+        ************************************************************************/      
+         
+        break; 
+    } 
+}   
+
+void ManageSystemPower() 
+{ 
+    /* Variable declarations */ 
+    CYBLE_BLESS_STATE_T blePower; 
+    uint8 interruptStatus ; 
+     
+    /* Disable global interrupts to avoid any other tasks from interrupting this 
+section of  
+    code*/ 
+    interruptStatus  = CyEnterCriticalSection(); 
+     
+    /* C9. Get current state of BLE sub system to check if it has successfully 
+entered deep  
+    sleep state */ 
+    blePower = CyBle_GetBleSsState(); 
+     
+    /* C10. System can enter Deep-Sleep only when BLESS and rest of the application 
+are in  
+    DeepSleep or equivalent power modes */ 
+    if((blePower == CYBLE_BLESS_STATE_DEEPSLEEP || blePower == 
+CYBLE_BLESS_STATE_ECO_ON) && applicationPower == DEEPSLEEP) 
+    { 
+        applicationPower = WAKEUP_DEEPSLEEP; 
+        /* C11. Put system into Deep-Sleep mode*/ 
+        CySysPmDeepSleep(); 
+    } 
+    /* C12. BLESS is not in Deep Sleep mode. Check if it can enter Sleep mode */ 
+    else if((blePower != CYBLE_BLESS_STATE_EVENT_CLOSE)) 
+    { 
+        /* C13. Application is in Deep Sleep. IMO is not required */ 
+        if(applicationPower == DEEPSLEEP) 
+        { 
+            applicationPower = WAKEUP_DEEPSLEEP; 
+             
+            /* C14. change HF clock source from IMO to ECO*/ 
+            CySysClkWriteHfclkDirect(CY_SYS_CLK_HFCLK_ECO);  
+            /* C14. stop IMO for reducing power consumption */ 
+            CySysClkImoStop();  
+            /*C15. put the CPU to sleep */ 
+            CySysPmSleep(); 
+            /* C16. starts execution after waking up, start IMO */ 
+            CySysClkImoStart(); 
+            /* C16. change HF clock source back to IMO */ 
+            CySysClkWriteHfclkDirect(CY_SYS_CLK_HFCLK_IMO); 
+        } 
+        /*C17. Application components need IMO clock */ 
+        else if(applicationPower == SLEEP ) 
+        { 
+            /* C18. Put the system into Sleep mode*/ 
+            applicationPower = WAKEUP_SLEEP; 
+            CySysPmSleep(); 
+        } 
+    } 
+     
+    /* Enable interrupts */ 
+    CyExitCriticalSection(interruptStatus ); 
+}
+
+int main()
+{
+ /* Variable declarations */
+ CYBLE_LP_MODE_T lpMode;
+ CYBLE_BLESS_STATE_T blessState;
+ uint8 interruptStatus;
+ /* Enable global interrupts */
+ CyGlobalIntEnable;
+ 
+ /* C1. Stop the ILO to reduce current consumption */
+ CySysClkIloStop();
+ /* C2. Configure the divider values for the ECO, so that a 3-MHz ECO divided 
+ clock can be provided to the CPU in Sleep mode */
+ CySysClkWriteEcoDiv(CY_SYS_CLK_ECO_DIV8);
+ 
+ /* Start the BLE Component and register the generic event handler */
+ apiResult = CyBle_Start(AppCallBack);
+ 
+ /* Wait for BLE Component to initialize */
+ while (CyBle_GetState() == CYBLE_STATE_INITIALIZING)
+ {
+ CyBle_ProcessEvents(); 
+ } 
+ /*Application-specific Component and other initialization code below */
+ applicationPower = ACTIVE;
+ 
+ /* main while loop of the application */
+ while(1)
+ {
+ /* Process all pending BLE events in the stack */
+ CyBle_ProcessEvents(); 
+ /* C3. Call the function that manages the BLESS power modes */
+ CyBle_EnterLPM(CYBLE_BLESS_DEEPSLEEP); 
+ /*C4. Run your application specific code here */
+ if(applicationPower == ACTIVE)
+ {
+ RunApplication();
+ }
+ 
+ /*C6. Manage Application power mode */
+ ManageApplicationPower(); 
+ 
+ /*C7. Manage System power mode */
+ ManageSystemPower();
+ }
+}
